efe16031247765bf9ad297608b77dd81717352be
Андрей Боев <a.boev.it@gmail.com>
2024-05-22 14:14:16 +0300

    Update: Удаление для исправления

--- 5/46963 (andrey).py
+++
@@ -1,7 +0,0 @@
-for num in range(511, 10000):
-    b = bin(num)[2:]
-    count_1 = b[1::2].count('1')
-    count_0 = b[0::2].count('0')
-    if abs(count_1 - count_0) == 5:
-        print(num)
-        break
